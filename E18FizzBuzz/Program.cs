5934ebca471628af4c25072a1582262e24a4e76f
Brian Neal <brianbneal@gmail.com>
2022-07-16 12:28:48 -0500

    Add project files.

---
+++ E18FizzBuzz/Program.cs
@@ -0,0 +1,19 @@
+﻿//FizzBuzz - Print integers 1 to 100,
+//but print “Fizz” if an integer is divisible by 3,
+//“Buzz” if an integer is divisible by 5,
+//and “FizzBuzz” if an integer is divisible by both 3 and 5.
+
+string[] fizz = new string[3] { "Fizz", "", "" };
+string[] buzz = new string[5] { "Buzz", "", "", "", "" };
+string valueToPrint;
+
+for (int i = 1; i <= 100; i++)
+{
+    valueToPrint = i.ToString().PadRight(8); //"3       "
+    valueToPrint += fizz[i % 3];//"3       Fizz"
+    valueToPrint += buzz[i % 5];
+
+    Console.WriteLine(valueToPrint.Substring(valueToPrint.Length - 8).Trim());
+}
+
+Console.ReadLine();
